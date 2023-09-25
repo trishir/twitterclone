6484d1b4efdde4fb572285c1b4470afa913caae0
Trishir <trishir2m.10@gmail.com>
2023-09-25 19:55:10 -0230

    Moved sidebar into diff file

---
+++ src/components/leftSidebar.tsx
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import {BiHomeCircle, BiUser} from 'react-icons/bi';
+import{HiOutlineHashtag} from 'react-icons/hi';
+import{HiEnvelope} from 'react-icons/hi2';
+import{
+  BsBell, 
+  BsBookmark, 
+  BsTwitter,
+  BsEnvelope,
+  BsThreeDots,
+} from 'react-icons/bs';
+
+const NAVIGATION_ITEMS = [
+    {
+        title: "Twitter",
+        icon: BsTwitter,
+    },
+    {
+        title:'Home',
+        icon:BiHomeCircle
+    },
+    {
+        title: "Explore",
+        icon: HiOutlineHashtag,
+    },
+    {
+        title: "Notifications",
+        icon: BsBell,
+    },
+    {
+        title: "Messages",
+        icon: BsEnvelope,
+    },
+    {
+        title: "Bookmarks",
+        icon: BsBookmark,
+    },
+    {
+        title: "Profile",
+        icon: BiUser,
+    }
+];
+
+const leftSidebar = () => {
+  return (
+    <section className="fixed w-[275px] flex flex-col h-screen items-stretch">
+          <div className="flex flex-col items-search h-full space-y-4 mt-4">
+            {NAVIGATION_ITEMS.map((item) => (
+              <Link 
+                href={"/${item.title.toLowerCase()}"} 
+                className="py-2 px-6 text-2xl hover:bg-white/10 transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-3xl"
+                key={item.title}
+                >
+                  <div>
+                    <item.icon />
+                  </div>
+                  {item.title !== "Twitter" && <div>{item.title}</div>}
+              </Link>
+            ))}
+            <button className="rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
+              Tweet
+            </button>
+          </div>
+          <button className="justify-between w-full rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-center hover:bg-white/10 transition duration-200">
+            <div className="flex items-center space-x-2">
+              <div className="rounded-full bg-slate-400 w-12 h-12"></div>
+              <div className="text-left text-sm">
+                <div className="font-semibold"> Trishir Singh </div>
+                <div>@trishir</div>
+              </div>
+            </div>
+            <div>
+              <BsThreeDots />
+            </div>
+          </button>
+        </section>
+  )
+}
+
+export default leftSidebar;
